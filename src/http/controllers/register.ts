478cfe1c20fb8e6c5a6f321fc554c1a52cfeca5f
Maria de Fátima <maria.cardoso@semantix.ai>
2023-03-14 12:21:17 -0300

    feat: :sparkles: create authenticate use case and tests

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -28,7 +28,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
         message: error.message,
       })
     }
-    return reply.status(500).send() // TODO fix me
+    throw error
   }
 
   return reply.status(201).send()
